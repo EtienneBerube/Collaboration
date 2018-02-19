eca15cbf88296b91885459dcba6ce951b17f3a37
EtienneBerube <tiennebrb@gmail.com>
2018-02-19 16:49:22 -0500

    DONE

--- CarRentalSystem.cpp
+++ CarRentalSystem.cpp
@@ -2,5 +2,22 @@
 // Created by Etienne on 19/02/2018.
 //
 
-#include "Car.h"
+
 #include "Customer_Objects/Customer.h"
+#include "Cars_objects/luxuryCar.h"
+#include "Cars_objects/regularCar.h"
+#include "Customer_Objects/RegularCustomer.h"
+#include "Customer_Objects/VIPCustomer.h"
+#include "Customer_Objects/CorporateCustomer.h"
+
+int main(){
+    Car c;
+    luxuryCar c1;
+    regularCar c2;
+
+    Customer c3;
+    RegularCustomer c5;
+    VIPCustomer c4;
+    CorporateCustomer c6;
+
+}
