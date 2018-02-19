632c9f8348671813adac356fce3ffe527f2dac85
EtienneBerube <tiennebrb@gmail.com>
2018-02-19 15:28:19 -0500

    Added all requires Customer

---
+++ Customer_Objects/VIPCustomer.h
@@ -0,0 +1,14 @@
+//
+// Created by Etienne on 19/02/2018.
+//
+
+#ifndef COLLABORATION_VIPCUSTOMER_H
+#define COLLABORATION_VIPCUSTOMER_H
+
+
+class VIPCustomer {
+
+};
+
+
+#endif //COLLABORATION_VIPCUSTOMER_H
