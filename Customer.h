6daac36fabf13533e8f7c13f2852eb91d60d0a06
Just Trying <justrying@github.com>
2018-02-19 15:22:43 -0500

    included new clas
    class customer

---
+++ Customer.h
@@ -0,0 +1,23 @@
+//
+// Created by Dean Chong San on 2018-02-19.
+//
+
+#ifndef COLLABORATION_CUSTOMER_H
+#define COLLABORATION_CUSTOMER_H
+
+#include <string>
+using std::string;
+
+class Customer {
+
+public :
+    Customer();
+    ~Customer();
+
+private:
+    string name;
+
+};
+
+
+#endif //COLLABORATION_CUSTOMER_H
