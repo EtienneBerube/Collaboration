76f173f7204a0d73b949a675d47f10df00c3a3d2
EtienneBerube <tiennebrb@gmail.com>
2018-03-17 10:48:24 -0400

    Override all abstract function from graph

--- assingment 4/A_Directional_Graph.cpp
+++ assingment 4/A_Directional_Graph.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "A_Directional_Graph.h"
+
+bool A_Directional_Graph::addVertex(Vertex &v) {
+    return false;
+}
+
+bool A_Directional_Graph::removeVertex(Vertex &v) {
+    return false;
+}
+
+bool A_Directional_Graph::addEdge(Node &e) {
+    return false;
+}
+
+bool A_Directional_Graph::remove(Node &e) {
+    return false;
+}
+
+bool A_Directional_Graph::searchVertex(const Vertex &v) {
+    return false;
+}
+
+bool A_Directional_Graph::searchEdge(const Node &e) {
+    return false;
+}
+
+void A_Directional_Graph::display(Vertex &v) const {
+
+}
+
+void A_Directional_Graph::display(Node &e) const {
+
+}
+
+void A_Directional_Graph::display() const {
+
+}
+
+std::string A_Directional_Graph::toString() const {
+    return nullptr;
+}
+
+bool A_Directional_Graph::clean() {
+    return false;
+}
--- assingment 4/A_Directional_Graph.h
+++ assingment 4/A_Directional_Graph.h
@@ -9,7 +9,28 @@
 #include "Graph.h"
 
 class A_Directional_Graph: public Graph {
+public:
+    bool addVertex(Vertex &v) override;
 
+    bool removeVertex(Vertex &v) override;
+
+    bool addEdge(Node &e) override;
+
+    bool remove(Node &e) override;
+
+    bool searchVertex(const Vertex &v) override;
+
+    bool searchEdge(const Node &e) override;
+
+    void display(Vertex &v) const override;
+
+    void display(Node &e) const override;
+
+    void display() const override;
+
+    std::string toString() const override;
+
+    bool clean() override;
 };
 
 
