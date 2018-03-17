41b4f8b1d43d1d9884b679bfc2e371944d9eeed5
EtienneBerube <tiennebrb@gmail.com>
2018-03-17 10:40:02 -0400

    Created required base class

---
+++ assingment 4/Edge.h
@@ -0,0 +1,14 @@
+//
+// Created by Etienne on 17/03/2018.
+//
+
+#ifndef ASSINGMENT_4_EDGE_H
+#define ASSINGMENT_4_EDGE_H
+
+
+class Edge {
+
+};
+
+
+#endif //ASSINGMENT_4_EDGE_H
---
+++ assingment 4/Vertex.h
@@ -0,0 +1,14 @@
+//
+// Created by Etienne on 17/03/2018.
+//
+
+#ifndef ASSINGMENT_4_VERTEX_H
+#define ASSINGMENT_4_VERTEX_H
+
+
+class Vertex {
+
+};
+
+
+#endif //ASSINGMENT_4_VERTEX_H
