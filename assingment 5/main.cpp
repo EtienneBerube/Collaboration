07718b020f1449cde4919de3559d38e6f7dbfa77
EtienneBerube <tiennebrb@gmail.com>
2018-03-28 21:42:41 -0400

    Added protection

--- assingment 5/main.cpp
+++ assingment 5/main.cpp
@@ -2,8 +2,15 @@
 #include <string>
 #include <vector>
 
+using namespace std;
 
 int main(int argc, char* argv[]) {
+
+    if (argc < 3 || argc >3) {
+        cout<<"Bad number of arguments. Expected 3, got "<<argc<<endl;
+        return 1;
+    }
+    
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
