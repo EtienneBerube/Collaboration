a754012b8a82a097905467b307556036e9674f2d
EtienneBerube <tiennebrb@gmail.com>
2018-03-28 21:51:31 -0400

    Added implementation of parameters

--- assingment 5/main.cpp
+++ assingment 5/main.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    string field;
+    string inputFile;
+    string outputFile;
 
-    if (argc < 3 || argc >3) {
+    if (argc < 4 || argc >4) {
         cout<<"Bad number of arguments. Expected 3, got "<<argc<<endl;
         return 1;
     }
-    
+
+    field = argv[1];
+    inputFile = argv[2];
+    outputFile = argv[3];
+
+    cout<<"1: "<<field<<" 2: "<<inputFile << " 3: "<<outputFile<<endl;
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
